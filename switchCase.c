ca18c5cf6cbed6fc3aa4802ea4a3c80e6376b29d
Er.Rohit Singh <contact.rohit56@gmail.com>
2017-06-02 16:20:19 +0530

    Create switchCase.c

---
+++ switchCase.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int weekday;
+    printf("Enter weekday ");
+    scanf("%d",&weekday);
+    switch(weekday)
+    {
+        case 1 : printf("Monday\n");
+        break;
+        case 2 : printf("Tuesday\n");
+        break;
+        case 3 : printf("Wednesday\n");
+        break;
+        case 4 : printf("Thursday\n");
+        break;
+        case 7 : printf("Sunday\n");
+        break;
+        case 6 : printf("Saturday\n");
+        break;
+        case 5 : printf("Friday\n");
+        break;    
+        default: printf("Invalid input\n");
+    }
+   return 0;
+}
+
