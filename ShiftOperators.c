efce5ec9854d089076587b9a6996925f9ab5a711
contactrohit56@gmail.com <contactrohit56@gmail.com>
2017-07-09 19:45:44 +0530

    add

---
+++ ShiftOperators.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	// Right shift  - divide by 2 R.D. Sharma
+	printf("%d\n",3>>1);
+	
+	//left shift - multiply by 2"
+	printf("%d",3<<1); 
+	return 0;
+}
